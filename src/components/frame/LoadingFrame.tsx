93ae546a4fb8f96539d09266656b1b4a1e3a90b2
egChoi92 <2013agal@gmail.com>
2023-10-16 16:42:27 +0900

    Design: 로딩 프레임 UI 개발

--- src/components/frame/LoadingFrame.tsx
+++ src/components/frame/LoadingFrame.tsx
@@ -1,3 +1,52 @@
+import styled, { keyframes } from 'styled-components';
+
+const KeyframesRotate = keyframes`
+  0% {
+	width: 0;
+  }
+  100% {
+	width: 100%;
+  }
+`;
+const StyledLoading = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
+const StyledLoadingBox = styled.div`
+	font-size: 0.875rem;
+	text-align: center;
+	color: #6d6d6d;
+`;
+
+const StyledLoadingBar = styled.div`
+	width: 100px;
+	height: 8px;
+	margin-top: 0.625rem;
+	border: 1px solid #6d6d6d;
+	border-radius: 4px;
+	position: relative;
+`;
+const StyledLoadingIcon = styled.div`
+	height: 100%;
+	border-radius: 0 5px 5px 0;
+	background-color: #3478f6;
+	animation: ${KeyframesRotate} 2s infinite linear;
+`;
+
 export default function LoadingFrame() {
-	return <p>정보를 불러오는 중입니다</p>;
+	return (
+		<StyledLoading>
+			<StyledLoadingBox>
+				Loading...
+				<StyledLoadingBar>
+					<StyledLoadingIcon />
+				</StyledLoadingBar>
+			</StyledLoadingBox>
+		</StyledLoading>
+	);
 }
